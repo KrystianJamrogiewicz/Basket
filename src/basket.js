ff00304a474d6f3fde692793af92bef2c02b6569
KrystianJamrogiewicz <krystianjamrogiewicz@gmail.com>
2024-03-20 19:29:11 +0100

    Add comments

--- src/basket.js
+++ src/basket.js
@@ -1,21 +1,22 @@
 // Stworzenie klasy Basket - koszyk
 class Basket {
+	// Konstruktor, każdy nowy obiekt typu Basket będzie posiadał właściwość: pusta tablice items.
 	constructor() {
-		this.items = [];
+		this.items = []; // this TRZEBA STOSOWAĆ W KONSTRUKTORZE, odnosi się do obiektu na którym zostało wywołane zdarzenie (nowy obiekt typu Basket).
 	}
 	// Skrócona metoda tworzenia konstruktora (działa tak jak ta po wyżej, ale jest mniej czytelna)
 	// items = [];
 
 	//Metoda dodawania przedmiotu do koszyka
 	add(item) {
-		this.items.push(item); // dodawanie przedmiotu do tablicy
+		this.items.push(item); // Dodawanie przedmiotu (item) do tablicy (items)
 	}
 
 	// Metoda obliczania całkowitej wartości koszyka
 	getTotalValue() {
 		// Zastosowanie metody reduce
 		// prev = poprzednia waartość, curr = obecna waartość, 0 = początekowa waartość
-		return this.items.reduce((prev, curr) => prev + curr.price, 0);
+		return this.items.reduce((prev, curr) => prev + curr.price, 0); // Zwróci sumę wszystkich cen prodktów w tablicy (items)
 
 		// Zastosowanie pętli for
 		/*
@@ -30,23 +31,23 @@ class Basket {
 
 	// Metoda wyświetlania listy przedmiotów
 	getBasketSummary() {
-		// Tablica items najpierw zostanie przetworzona przez map a potem przez forEach
+		// map zwróci nową tablicę z dwoma kolumnami (id, text), która następnie zostanie zwrócona przez metode getBasketSummary
 		return this.items.map((product, i) => {
 			return {
-				id: i + 1,
-				text: `${i + 1}. ${product.name} - ${product.price.toFixed(2)} PLN`,
+				id: i + 1, // Uzyskanie id produktu z indeksu tablicy
+				text: `${i + 1}. ${product.name} - ${product.price.toFixed(2)} PLN`, //Wypisanie atrybutów produktu
 			};
 		});
 	}
 
 	// Metoda usuwania przedmiotu z koszyka
 	remove(no) {
-		this.items.splice(no - 1, 1); // usuwanie przedmiotu o podanym indeksie i = indeks od którego zacznie się usówanie, 1 = ilość przedmiotów do usunięcia
+		this.items.splice(no - 1, 1); // Usuwanie przedmiotu o podanym indeksie i = indeks od którego zacznie się usówanie, 1 = ilość przedmiotów do usunięcia
 	}
 
 	// Metoda czyszcząca koszyk
 	clear() {
-		this.items.length = 0;
+		this.items.length = 0; // Ustawia długość tablicy na 0
 		// this.items = [];
 		// this.items.splice(0);
 	}
@@ -58,7 +59,7 @@ class Product {
 		this.name = productName;
 		this.price = productPrice;
 	}
-	// Metoda dodawania nowej ceny
+	// Metoda zmiany ceny
 	setNewPrice(newPrice) {
 		this.price = newPrice;
 	}
--- src/ui.js
+++ src/ui.js
@@ -1,56 +1,68 @@
-const buyBtns = [...document.querySelectorAll("[data-price]")]; //Zamienienie alementu NodeList na tablice [...]
-const basketUl = document.querySelector(".basket-list");
+const buyBtns = [...document.querySelectorAll("[data-price]")]; // Zamienienie elementu NodeList na tablice [...]. Tworzy tablicę buyBtns zawierającą wszystkie przyciski zakupu z atrybutem data-price.
+const basketUl = document.querySelector(".basket-list"); // Przypisanie do stałej basketUl elementu z dokumentu HTML o klasie: basket-list
 const buyAllBtn = document.querySelector(".btn-buy-all");
 
-const basket = new Basket();
+const basket = new Basket(); // Tworzy nowy obiekt (koszyk) na bazie klasy: Basket
 
+// Funkcja usuwania przedmiotu
 const removeItem = event => {
 	const id = Number(event.target.dataset.id);
-	basket.remove(id);
-	createBasketUi();
+	// event.target odnosi się do elementu, na którym zostało wywołane zdarzenie (w tym przypadku zdarzenie removeItem).
+	// event.target.dataset daje dostęp do właściwości dataset elementu docelowego, która zawiera wszystkie atrybuty danych zdefiniowane na tym elemencie.
+	// event.target.dataset.id pobiera wartość atrybutu data-id z elementu docelowego (na którym zostało wywołane zdarzenie removeItem).
+	// Number() zamienia typ danych na number.
+	basket.remove(id); // Wywołanie metody remove z klasy Basket (usuwanie przedmiotu z koszyka)
+	createBasketUi(); // Wywołanie metody createBasketUi (wyswietlanie listy przedmiotów w koszyku)
 };
 
-const createBasketUi = () => {
-	basketUl.innerText = "";
-	for (const { id, text } of basket.getBasketSummary()) {
-		const newLi = document.createElement("li");
-		newLi.innerText = text;
-		newLi.addEventListener("click", removeItem);
-		newLi.dataset.id = id;
-		basketUl.appendChild(newLi);
-	}
-	const basketTotalValue = basket.getTotalValue();
-
-	buyAllBtn.innerText = `Złóż zamówienie na kwotę:  ${basketTotalValue.toFixed(
-		2
-	)}`;
-
-	if (basketTotalValue > 0) {
-		buyAllBtn.removeAttribute("disabled");
-	} else {
-		buyAllBtn.setAttribute("disabled", "true");
-	}
-};
-
+// Funkcja dodawania prodktów do koszyka
 function addProductToBasket(event) {
 	const name = event.target.dataset.name;
 	const price = Number(event.target.dataset.price);
-	//Element na którym został wywołany event, dataset to atrybuty data-nazwa
-	const newProduct = new Product(name, price);
-	basket.add(newProduct);
-	createBasketUi();
+	const newProduct = new Product(name, price); // Stworzenie nowego obiektu klasy Product
+	basket.add(newProduct); // Dodanie nowego przedmiotu do koszyka, wywołanie metody add z klasy Basket
+	createBasketUi(); // Wywołanie metody createBasketUi (wyswietlanie listy przedmiotów w koszyku)
 }
 
+// Funkcja tworzenia listy przedmiotów w koszyku
+const createBasketUi = () => {
+	basketUl.innerText = ""; // Czyszczenie listy przedmiotów o klasie basket-list w koszyku.
+	for (const { id, text } of basket.getBasketSummary()) {
+		// Przypisanie do stałej id i text wartości zwrócone przez metode getBasketSummary klasy Basket.
+		const newLi = document.createElement("li"); // Stworzenie nowego elementu listy li
+		newLi.innerText = text; // Wpisanie do nowego elementu listy li tekstu (nazwy przedmiotu i wartości)
+		newLi.addEventListener("click", removeItem); // Dodanie do nowego elementu listy li zdarzenia klikniecia (wywołanie metody removeItem)
+		newLi.dataset.id = id; // Dodanie do nowego elementu listy li atrybutu data-id (id przedmiotu)
+		basketUl.appendChild(newLi); // Dodanie do listy przedmiotów o klasie basket-list nowego elementu listy li
+	}
+
+	const basketTotalValue = basket.getTotalValue(); // Przypisanie do stałej basketTotalValue aktualnej wartości koszyka
+
+	// Dodanie atrybutu disabled do przycisku o klasie: buyAllBtn
+	buyAllBtn.innerText = `Złóż zamówienie na kwotę:  ${basketTotalValue.toFixed(
+		2
+	)}`; // Napis na przycisku
+
+	// Sprawdzenie czy koszyk nie jest pusty
+	if (basketTotalValue > 0) {
+		buyAllBtn.removeAttribute("disabled"); // Usuniecie atrybutu disabled (aktywny) z przycisku
+	} else {
+		buyAllBtn.setAttribute("disabled", "true"); // Dodanie atrybutu disabled do przycisku
+	}
+};
+
+// Funkcja wywoływana w momencie klikniecia przycisku Złóż zamówienie
 const buyAllProducts = () => {
 	alert(
 		`Złożyłeś zamówienie na kwotę: ${basket.getTotalValue().toFixed(2)} zł.`
-	);
-	basket.clear();
-	createBasketUi();
+	); // Wyswietlenie alertu imitującego zakup
+	basket.clear(); // Czyszczenie koszyka
+	createBasketUi(); // Wywołanie metody createBasketUi (wyswietlanie listy przedmiotów w koszyku)
 };
 
+// Pętla iterująca po tablicy wszystkich przycisków o atrybucie data-price, nadaje im nazwę btn i dodaje do nich zdarzenie klikniecia.
 for (const btn of buyBtns) {
-	btn.addEventListener("click", addProductToBasket);
+	btn.addEventListener("click", addProductToBasket); // Dodanie do przycisku zdarzenia klikniecia, które wywoła funkcje: addProductToBasket.
 }
 
-buyAllBtn.addEventListener("click", buyAllProducts);
+buyAllBtn.addEventListener("click", buyAllProducts); // Dodanie do przycisku buyAllBtn zdarzenia klikniecia, które wywoła funkcje: buyAllProducts.
